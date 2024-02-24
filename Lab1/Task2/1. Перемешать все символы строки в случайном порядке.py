091ec75cc5851bdd72d1ecbc719a16ab6a1d9cd8
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2024-02-24 18:30:32 +0300

    Create 1. Перемешать все символы строки в случайном порядке.py

---
+++ Lab1/Task2/1. Перемешать все символы строки в случайном порядке.py
@@ -0,0 +1,8 @@
+import random
+def swap(n):
+    slovo=n
+    slovo_list=list(slovo)
+    swapping=random.sample(slovo_list,len(slovo_list))
+    return swapping
+n = input("Введите строку: ")
+print(uporyad(n))
