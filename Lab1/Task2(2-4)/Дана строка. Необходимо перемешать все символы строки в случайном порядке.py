09df4bda4cfd787aff88e88059246dc32a749b4e
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2024-02-24 18:28:52 +0300

    Delete Lab1/Task2(2-4) directory

--- Lab1/Task2(2-4)/Дана строка. Необходимо перемешать все символы строки в случайном порядке.py
+++
@@ -1,8 +0,0 @@
-import random
-def swap(n):
-    slovo=n
-    slovo_list=list(slovo)
-    swapping=random.sample(slovo_list,len(slovo_list))
-    return swapping
-n=input("Введите строку: ")
-print(swap(n))
